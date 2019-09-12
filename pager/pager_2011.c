27e9f8b29e39c4661da0453c78860be160c6db56
honeylife <zzq921108@126.com>
2019-09-12 10:58:56 +0800

    文件中查找信息

--- pager/pager_2011.c
+++ pager/pager_2011.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
 // 2011 年 C语言试卷
 
@@ -23,9 +25,93 @@ void fun1(){
     // 总结，这里比来三个放循环要来的好，首先时间复杂度是明显的。
 }
 
+/**
+ * 输入三条边，判断是否为三角形，是等边三角形还是等腰三角形
+ * */
+ void fun2(){
+     int a,b,c;
+     printf("please input a,b,c:\n");
+     scanf("%d %d %d",&a,&b,&c);
+     if (a+b>c&&a+c>b&&b+c>a)
+     {
+         printf("This is a Triangle\n");
+     }else
+     {
+         printf("This is not a Triangle");
+         return;
+     }
+     
+     if (a==b&&b==c)
+     {
+        printf("This is a equilateral triangle \n");
+        return;
+     }
+     
+     if (a==b&&b!=c)
+     {
+          printf("This is a  isosceles triangle  \n");
+     }
+     if (a==c&&b!=c)
+     {
+         printf("This is a  isosceles triangle  \n");
+     }
+     
+     if (b==c&&a!=b)
+     {
+         printf("This is a  isosceles triangle  \n");
+     }
+     
+     
+     
+ }
+ /**
+  * 从键盘输入 员工信息，在member.bat 文件中查找，如果存在，则返回员工信息，如果没有在，在磁盘创建该文件
+  * */
+ void fun3(){
+     FILE *fp;
+     char *name,*fname;
+     int flag=0;
+     printf("please name:\n");
+     gets(name);
+    //  if ((fp=fopen("member.dat","r"))==NULL)
+    //  {
+    //      printf("file is error!");
+    //      getch();
+    //      exit(1);
+    //  }
+   fp=fopen("member.dat","r");
+    while (!feof(fp))
+    {
+       fscanf(fp,"%s",fname);
+       if (strcmp(fname,name)==0)
+       {
+           printf("%s exist",name);
+            flag=1;
+            break;
+       }
+       
+    }
+     fclose(fp);
+    if (flag==0)
+    {
+        printf("create member.dat and join name\n");
+        fp=fopen("member.dat","a");
+        fprintf(fp,"%s",name);
+        fclose(fp);
+    }
+    
+    
+
+     
+     
+ }
+
 int main(int argc, char const *argv[])
 {
-    fun1();
+    // fun1();
+    // fun2();
+    // fun3();
+    fun3();
     return 0;
 }
 
