016f87f41f66e1f7e1b3b30862f97150c73f26e2
honeylife <zzq921108@126.com>
2019-09-19 10:59:52 +0800

    欧几里算法 求最大公因子

---
+++ pager/pager_2000.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct node
+{
+    int data;
+    struct node *link;
+} Node, *LinkList;
+
+void CHANGE() {}
+
+/**
+ * 欧几里得算法  辗转相除法 求最大因子
+ * */
+int fun(int m, int n)
+{
+    if (n == 0)
+    {
+        return m;
+    }
+    if (m < n)
+    {
+        return fun(n, m);
+    }
+
+    return  fun(n, m % n);
+}
+
+int fun2(int m,int n){
+
+    int temp;
+    while (1)
+    {
+       
+        if (m<n)
+        {
+            temp=m;
+            m= n;
+            n=temp; 
+        }
+
+         if(n==0){
+            return m;
+        }
+        else
+        {
+            int r = m % n;  
+                m = n;  
+                n = r;  
+        }
+    
+    }
+    
+}
+int main(int argc, char const *argv[])
+{
+
+    int a = fun(12, 8);
+    printf("%d\n", a);
+      int aa = fun2(12, 8);
+    printf("aa=%d\n", aa);
+    return 0;
+}
