8eeb0ba77930f37473e915445f78dff9f8e36f8c
honeylife <zzq921108@126.com>
2019-10-08 17:54:57 +0800

    n

---
+++ pager/2005/pager_2005_two.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+// 输入n 输出n以内的素数
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("please input n:\n");
+    scanf("%d", &n);
+    int flag = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        flag = 0;
+        for (int j = 2; j <i/2 ; j++)
+        {
+            if (i % j == 0)
+            {
+                flag=0;
+                break;
+            }
+            else
+            {
+                flag = 1;
+            }
+        }
+        if (flag == 1)
+        {
+            printf("%d,", i);
+        }
+    }
+
+    return 0;
+}
